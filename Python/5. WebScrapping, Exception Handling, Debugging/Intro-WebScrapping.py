687b088bdbda7a0fef13c86c501f1a6a47d2287e
Gurjas Singh <gurjassingh92@gmail.com>
2019-06-26 18:05:03 +0530

    Update Intro-WebScrapping.py

--- Python/5. WebScrapping, Exception Handling, Debugging/Intro-WebScrapping.py
+++ Python/5. WebScrapping, Exception Handling, Debugging/Intro-WebScrapping.py
@@ -1,30 +1,40 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
+## defining website address, from where to scrape data
 website_address = "http://quotes.toscrape.com"
-result = requests.get(website_address, verify=False)
+result = requests.get(website_address)
+# print(result)
 
 soup = BeautifulSoup(result.text, "html.parser")
-# print(soup.prettify())
+# print(soup)
 
-quotes = soup.findAll("div", {"class": "quote"})
-# print(quotes)
-i = 0
-for quote in quotes:
-    quotation = quote.find("span", {"class": "text"}).text
-    author = quote.find("small", {"class": "author"}).text
-    tags = quote.find("div", {"class": "tags"})
-    tags_list = []
-    all_values = {}
+quotes_list = soup.findAll("div", {"class": "quote"})
+# print(quotes_list)
 
-    for tag in tags.find_all('a'):
-        tags_list.append(tag.text)
+quotations_list = []
+authors_list = []
+tags_list = []
+for quote in quotes_list:
+    quotation = quote.find("span", {"class": "text"})
+    quotations_list.append(quotation.text)
 
-    all_values[i] = {
-        'Quote' : quotation,
-        'Author' : author,
-        'Tags' : tags_list
-    }
-    i = i+1
+    author = quote.find("small", {"class": "author"})
+    authors_list.append(author.text)
 
-    print(all_values)
+    tags = quote.findAll("a", {"class": "tag"})
+    new_tags  = ""
+    for tag in tags:
+        new_tags = new_tags + tag.text + ","
+    tags_list.append(new_tags)
+
+quotation_dict = {
+    'Quotation': quotations_list,
+    'Author': authors_list,
+    'Tags': tags_list
+}
+
+quotation_dict = pd.DataFrame(quotation_dict)
+print(quotation_dict)
+quotation_dict.to_csv("NewQuotations.csv")
